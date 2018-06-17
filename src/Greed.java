9c8c9479168ffa69863ce5ce5d566c06fa4824a1
Justin Cooksey <>
2018-06-17 21:51:39 +1000

    Greed Completed
    Greed completed although straight needs better definition.

--- src/Greed.java
+++ src/Greed.java
@@ -3,35 +3,38 @@ public class Greed {
     public int score(int[] dice) {
         int x;
         int result = 0;
-        int ones = 0;
-        int twos = 0;
-        int threes = 0;
-        int fours = 0;
-        int fives = 0;
-        int sixes = 0;
+        int threePair = 0;
+        int straight = 0;
+        int[] numbers = new int[]{0,0,0,0,0,0,0};
 
         int die = dice.length;
         if(die < 1) return(result);
 
-        for(x=0; x<die; x++) {
-            if(dice[x] == 1) ones++;
-            if(dice[x] == 2) twos++;
-            if(dice[x] == 3) threes++;
-            if(dice[x] == 4) fours++;
-            if(dice[x] == 5) fives++;
-            if(dice[x] == 6) sixes++;
+        for(x=0; x<die; x++) numbers[dice[x]]++;
+
+        if(numbers[1] == 1) result += 100;
+        if(numbers[5] == 1) result += 50;
+
+        if(numbers[1] >= 3) result += 1000;
+        if(numbers[2] >= 3) result += 200;
+        if(numbers[3] >= 3) result += 300;
+        if(numbers[4] >= 3) result += 400;
+        if(numbers[5] >= 3) result += 500;
+        if(numbers[6] >= 3) result += 600;
+
+        for(x=1; x<7; x++) {
+            if(numbers[x] == 4) result *= 2;
+            if(numbers[x] == 5) result *= 4;
+            if(numbers[x] == 6) result *= 8;
+            if(numbers[x] == 2) threePair++;
+            if(numbers[x] == 1) straight++;
         }
 
-        if(ones == 1) result += 100;
-        if(fives == 1) result += 50;
-        if(ones == 3) result += 1000;
-        if(twos == 3) result += 200;
-        if(threes == 3) result += 300;
-        if(fours == 3) result += 400;
-        if(fives == 3) result += 500;
-        if(sixes == 3) result += 600;
+        if(threePair == 3) result += 800;
+        if(straight == 6) result += 1200;
 
         return(result);
+
     }
 
 }
--- test/GreedTest.java
+++ test/GreedTest.java
@@ -36,8 +36,32 @@ class GreedTest {
         assertEquals(1000,game.score(dice1));
         int[] dice2 = new int[]{1,1,1};
         assertEquals(1000,game.score(dice2));
-
     }
+
+    @Test
+    void score_Four1() {
+        int[] dice1 = new int[]{1,1,1,3,1,4};
+        assertEquals(2000,game.score(dice1));
+        int[] dice2 = new int[]{1,1,1,1};
+        assertEquals(2000,game.score(dice2));
+    }
+
+    @Test
+    void score_Five1() {
+        int[] dice1 = new int[]{1,2,1,1,1,1};
+        assertEquals(4000,game.score(dice1));
+        int[] dice2 = new int[]{1,1,1,1,1};
+        assertEquals(4000,game.score(dice2));
+    }
+
+    @Test
+    void score_Six1() {
+        int[] dice1 = new int[]{1,1,1,1,1,1};
+        assertEquals(8000,game.score(dice1));
+        int[] dice2 = new int[]{1,1,1,1,1,1};
+        assertEquals(8000,game.score(dice2));
+    }
+
 
     @Test
     void score_Three2() {
@@ -45,7 +69,30 @@ class GreedTest {
         assertEquals(200,game.score(dice1));
         int[] dice2 = new int[]{2,2,2};
         assertEquals(200,game.score(dice2));
+    }
 
+    @Test
+    void score_Four2() {
+        int[] dice1 = new int[]{2,2,2,3,2,4};
+        assertEquals(400,game.score(dice1));
+        int[] dice2 = new int[]{2,2,2,2};
+        assertEquals(400,game.score(dice2));
+    }
+
+    @Test
+    void score_Five2() {
+        int[] dice1 = new int[]{2,2,2,2,2,4};
+        assertEquals(800,game.score(dice1));
+        int[] dice2 = new int[]{2,2,2,2,2};
+        assertEquals(800,game.score(dice2));
+    }
+
+    @Test
+    void score_Six2() {
+        int[] dice1 = new int[]{2,2,2,2,2,2};
+        assertEquals(1600,game.score(dice1));
+        int[] dice2 = new int[]{2,2,2,2,2,2};
+        assertEquals(1600,game.score(dice2));
     }
 
     @Test
@@ -57,12 +104,62 @@ class GreedTest {
     }
 
     @Test
+    void score_Four3() {
+        int[] dice1 = new int[]{3,3,3,3,2,4};
+        assertEquals(600,game.score(dice1));
+        int[] dice2 = new int[]{3,3,3,3};
+        assertEquals(600,game.score(dice2));
+    }
+
+    @Test
+    void score_Five3() {
+        int[] dice1 = new int[]{3,3,3,3,3,4};
+        assertEquals(1200,game.score(dice1));
+        int[] dice2 = new int[]{3,3,3,3,3};
+        assertEquals(1200,game.score(dice2));
+    }
+
+    @Test
+    void score_Six3() {
+        int[] dice1 = new int[]{3,3,3,3,3,3};
+        assertEquals(2400,game.score(dice1));
+        int[] dice2 = new int[]{3,3,3,3,3,3};
+        assertEquals(2400,game.score(dice2));
+    }
+
+
+    @Test
     void score_Three4() {
         int[] dice1 = new int[]{4,3,6,4,2,4};
         assertEquals(400,game.score(dice1));
         int[] dice2 = new int[]{4,4,4};
         assertEquals(400,game.score(dice2));
     }
+
+    @Test
+    void score_Four4() {
+        int[] dice1 = new int[]{4,4,6,4,2,4};
+        assertEquals(800,game.score(dice1));
+        int[] dice2 = new int[]{4,4,4,4};
+        assertEquals(800,game.score(dice2));
+    }
+
+    @Test
+    void score_Five4() {
+        int[] dice1 = new int[]{4,4,4,4,2,4};
+        assertEquals(1600,game.score(dice1));
+        int[] dice2 = new int[]{4,4,4,4,4};
+        assertEquals(1600,game.score(dice2));
+    }
+
+    @Test
+    void score_Six4() {
+        int[] dice1 = new int[]{4,4,4,4,4,4};
+        assertEquals(3200,game.score(dice1));
+        int[] dice2 = new int[]{4,4,4,4,4,4};
+        assertEquals(3200,game.score(dice2));
+    }
+
 
     @Test
     void score_Three5() {
@@ -73,12 +170,69 @@ class GreedTest {
     }
 
     @Test
+    void score_Four5() {
+        int[] dice1 = new int[]{5,5,5,5,2,4};
+        assertEquals(1000,game.score(dice1));
+        int[] dice2 = new int[]{5,5,5,5};
+        assertEquals(1000,game.score(dice2));
+    }
+
+    @Test
+    void score_Five5() {
+        int[] dice1 = new int[]{5,5,5,5,5,4};
+        assertEquals(2000,game.score(dice1));
+        int[] dice2 = new int[]{5,5,5,5,5};
+        assertEquals(2000,game.score(dice2));
+    }
+    @Test
+    void score_Six5() {
+        int[] dice1 = new int[]{5,5,5,5,5,5};
+        assertEquals(4000,game.score(dice1));
+        int[] dice2 = new int[]{5,5,5,5,5,5};
+        assertEquals(4000,game.score(dice2));
+    }
+
+
+    @Test
     void score_Three6() {
         int[] dice1 = new int[]{6,3,6,6,2,4};
         assertEquals(600,game.score(dice1));
         int[] dice2 = new int[]{6,6,6};
         assertEquals(600,game.score(dice2));
     }
+
+    @Test
+    void score_Four6() {
+        int[] dice1 = new int[]{6,3,6,6,6,4};
+        assertEquals(1200,game.score(dice1));
+        int[] dice2 = new int[]{6,6,6,6};
+        assertEquals(1200,game.score(dice2));
+    }
+
+    @Test
+    void score_Five6() {
+        int[] dice1 = new int[]{6,6,6,6,6,4};
+        assertEquals(2400,game.score(dice1));
+        int[] dice2 = new int[]{6,6,6,6,6};
+        assertEquals(2400,game.score(dice2));
+    }
+
+    @Test
+    void score_Six6() {
+        int[] dice1 = new int[]{6,6,6,6,6,6};
+        assertEquals(4800,game.score(dice1));
+        int[] dice2 = new int[]{6,6,6,6,6,6};
+        assertEquals(4800,game.score(dice2));
+    }
+
+    @Test
+    void score_Straight() {
+        int[] dice1 = new int[]{1,2,3,4,5,6};
+        assertEquals(1350,game.score(dice1));
+        int[] dice2 = new int[]{3,4,5,1,2,6};
+        assertEquals(1350,game.score(dice2));
+    }
+
 
 
 }
